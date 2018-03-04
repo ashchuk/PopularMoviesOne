a7fd37ecc2be88a3ad876dfc87d920dd21079004
ashchuk <androidakbluebox@gmail.com>
2018-03-04 15:40:02 +0300

    Add internet permission, make test api call

--- app/src/main/java/com/ashchuk/popularmoviesone/ui/MainPage/MainPageActivity.java
+++ app/src/main/java/com/ashchuk/popularmoviesone/ui/MainPage/MainPageActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.ashchuk.popularmoviesone.R;
 import com.ashchuk.popularmoviesone.api.IMovieDBApi;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -14,6 +15,8 @@ import dagger.android.support.DaggerAppCompatActivity;
 import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainPageActivity extends DaggerAppCompatActivity implements IMainPageView {
@@ -32,6 +35,29 @@ public class MainPageActivity extends DaggerAppCompatActivity implements IMainPa
         IMovieDBApi movieDBApi = retrofit.create(IMovieDBApi.class);
 
         Call<ResponseBody> test = movieDBApi.getTopRated();
+        test.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful()) {
+                    try {
+                        String values = response.body().string();
+                        Log.v("TEST", "getTopRated query successfull");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.v("TEST", "getTopRated query failure");
+                    }
+                } else {
+                    System.out.println(response.errorBody());
+                    Log.v("TEST", "getTopRated response is not successfull");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.v("TEST", "getTopRated onFailure");
+                t.printStackTrace();
+            }
+        });
         Call<ResponseBody> test2 = movieDBApi.getPopular();
         Call<ResponseBody> test3 = movieDBApi.getMovieInfo("500");
     }
