08b35552e35669b9b7dc0cc4fca03b89ef335001
ashchuk <androidakbluebox@gmail.com>
2018-03-14 18:38:59 +0300

    Add API_KEY constant

--- app/src/main/java/com/ashchuk/popularmoviesone/utils/Constants.java
+++ app/src/main/java/com/ashchuk/popularmoviesone/utils/Constants.java
@@ -10,4 +10,5 @@ public class Constants {
     public static final String MOVIE_INFO= "{movie_id}";
     public static final String END_POINT = "https://api.themoviedb.org/3/movie/";
     public static final String POSTER_END_POINT = "http://image.tmdb.org/t/p/w342";
+    public static final String API_KEY = "TOP_SECRET";
 }
